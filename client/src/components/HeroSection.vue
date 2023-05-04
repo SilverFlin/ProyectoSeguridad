81584b261361d00d4c5453f39c57455f431c4d85
SilverFlin <toledorusso@outlook.com>
2023-05-04 11:56:43 -0700

    feat: texto en codigo ejemplo hero section

--- client/src/components/HeroSection.vue
+++ client/src/components/HeroSection.vue
@@ -83,15 +83,18 @@
                         <div
                           class="px-4 py-2 text-white border-b border-r border-b-white/20 border-r-white/10 bg-white/5"
                         >
-                          NotificationSetting.jsx
+                          SeguridadInformatica.ts
                         </div>
                         <div class="px-4 py-2 border-r border-gray-600/10">
-                          App.jsx
+                          DropDatabaseCia.sql
                         </div>
                       </div>
                     </div>
                     <div class="px-6 pt-6 pb-14">
-                      <!-- Your code example -->
+                      <p class="text-gray-400">
+                        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA<br />
+                        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                      </p>
                     </div>
                   </div>
                 </div>
