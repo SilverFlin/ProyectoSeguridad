fef743d4be2e8f6f985ab7100e370bcad7725585
SilverFlin <toledorusso@outlook.com>
2023-05-04 19:08:37 -0700

    chore: remover gatito prueba

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,25 +1,7 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+import { RouterView } from "vue-router";
+
 import Header from "./components/Header.vue";
-import { encriptarRequest } from "./modules/EncryptServices";
-import { ref } from "vue";
-import type { ButtonHTMLAttributes, ImgHTMLAttributes } from "vue";
-
-const isTestBtnToggled = ref(false);
-const tuHashSir = ref("");
-
-const toggleTest = () => {
-  encriptarRequest("SF")
-    .then((res) => {
-      console.log(res);
-      isTestBtnToggled.value = !isTestBtnToggled.value;
-      tuHashSir.value = `Tu hash: ${res.data.texto}`;
-    })
-    .catch(() => {
-      tuHashSir.value = "Error xd";
-    });
-};
 </script>
 
 <template>
@@ -27,18 +9,5 @@ const toggleTest = () => {
     <Header></Header>
 
     <RouterView />
-    <div @click="toggleTest">
-      <img
-        style="height: 100px; width: 100px"
-        v-if="isTestBtnToggled"
-        src="@/assets/images/cute-cat.gif"
-      />
-      <img
-        style="height: 100px; width: 100px"
-        v-if="!isTestBtnToggled"
-        src="@/assets//images/kitty-cat-sandwich.gif"
-      />
-      <p>{{ tuHashSir }}</p>
-    </div>
   </div>
 </template>
