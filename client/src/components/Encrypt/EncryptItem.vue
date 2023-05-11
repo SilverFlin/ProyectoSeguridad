4073fa3dffa69125596c1f60b1bb635649452fe0
SilverFlin <toledorusso@outlook.com>
2023-05-11 08:03:10 -0700

    feat: modals

--- client/src/components/Encrypt/EncryptItem.vue
+++ client/src/components/Encrypt/EncryptItem.vue
@@ -30,23 +30,103 @@
       />
     </div>
   </div>
+  <TransitionRoot as="template" :show="open">
+    <Dialog as="div" class="relative z-10" @close="open = false">
+      <TransitionChild
+        as="template"
+        enter="ease-out duration-300"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="ease-in duration-200"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div
+          class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75"
+        />
+      </TransitionChild>
+
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div
+          class="flex items-end justify-center min-h-full p-4 text-center sm:items-center sm:p-0"
+        >
+          <TransitionChild
+            as="template"
+            enter="ease-out duration-300"
+            enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enter-to="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leave-from="opacity-100 translate-y-0 sm:scale-100"
+            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <DialogPanel
+              class="relative px-4 pt-5 pb-4 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:w-full sm:max-w-lg sm:p-6"
+            >
+              <div class="sm:flex sm:items-start">
+                <div
+                  class="flex items-center justify-center flex-shrink-0 w-12 h-12 mx-auto bg-red-100 rounded-full sm:mx-0 sm:h-10 sm:w-10"
+                >
+                  <ExclamationTriangleIcon
+                    class="w-6 h-6 text-red-600"
+                    aria-hidden="true"
+                  />
+                </div>
+                <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                  <DialogTitle
+                    as="h3"
+                    class="text-base font-semibold leading-6 text-gray-900"
+                    >Algo salió mal</DialogTitle
+                  >
+                  <div class="mt-2">
+                    <p class="text-sm text-gray-500">
+                      Funcionalidad no disponible, inicia sesión o intenta más
+                      tarde.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  class="inline-flex justify-center w-full px-3 py-2 text-sm font-semibold text-white bg-red-600 rounded-md shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                  @click="open = false"
+                >
+                  OK
+                </button>
+              </div>
+            </DialogPanel>
+          </TransitionChild>
+        </div>
+      </div>
+    </Dialog>
+  </TransitionRoot>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import { encriptarRequest } from "../../modules/EncryptServices";
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  TransitionChild,
+  TransitionRoot,
+} from "@headlessui/vue";
+import { ExclamationTriangleIcon } from "@heroicons/vue/24/outline";
+
+const open = ref(false);
 
 const textoEncriptar = ref("");
 const textoEncriptado = ref("");
 
 async function encriptar() {
-  await encriptarRequest(textoEncriptar.value)
-    .then((res) => {
+  try {
+    await encriptarRequest(textoEncriptar.value).then((res) => {
       const hash = res.data.hash;
       textoEncriptado.value = hash;
-    })
-    .catch(() => {
-      alert("Inicia Sesión");
     });
+  } catch (e) {
+    open.value = true;
+  }
 }
 </script>
